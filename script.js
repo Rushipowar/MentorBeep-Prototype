55adb04393a540818ba200331b3fe8109295e54e
Rushikesh Powar <114715746+Rushipowar@users.noreply.github.com>
2024-02-21 02:02:33 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+const Programms = document.querySelector(".program-course")
+
+function Programs(){
+    Programms.classList.remove("program-course")
+    Programms.classList.add("program-course-show")
+}
